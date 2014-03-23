c8d55f1a2e683c64a6027a1a4f36e836ce21e463
Vaerius <b.schelstraete@live.be>
2014-03-23 17:41:46 +0100

    cleaned up UI of login a bit

--- AllChat/UI/frmMain.Designer.cs
+++ AllChat/UI/frmMain.Designer.cs
@@ -40,6 +40,12 @@
             this.lblUsername = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtUsername = new System.Windows.Forms.TextBox();
+            this.pnlRegister = new System.Windows.Forms.Panel();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblNew = new System.Windows.Forms.Label();
+            this.lblNewUser = new System.Windows.Forms.Label();
+            this.lblInvalid1 = new System.Windows.Forms.Label();
+            this.lblInvalid2 = new System.Windows.Forms.Label();
             this.mnsMain.SuspendLayout();
             this.pnlLogin.SuspendLayout();
             this.SuspendLayout();
@@ -51,7 +57,7 @@
             this.tmiAbout});
             this.mnsMain.Location = new System.Drawing.Point(0, 0);
             this.mnsMain.Name = "mnsMain";
-            this.mnsMain.Size = new System.Drawing.Size(684, 24);
+            this.mnsMain.Size = new System.Drawing.Size(556, 24);
             this.mnsMain.TabIndex = 0;
             this.mnsMain.Text = "Mainmenu";
             // 
@@ -92,6 +98,14 @@
             // 
             // pnlLogin
             // 
+            this.pnlLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlLogin.Controls.Add(this.lblInvalid2);
+            this.pnlLogin.Controls.Add(this.lblInvalid1);
+            this.pnlLogin.Controls.Add(this.lblNewUser);
+            this.pnlLogin.Controls.Add(this.lblNew);
+            this.pnlLogin.Controls.Add(this.lblDescription);
             this.pnlLogin.Controls.Add(this.btnLogin);
             this.pnlLogin.Controls.Add(this.lblPassword);
             this.pnlLogin.Controls.Add(this.lblUsername);
@@ -99,12 +113,12 @@
             this.pnlLogin.Controls.Add(this.txtUsername);
             this.pnlLogin.Location = new System.Drawing.Point(12, 27);
             this.pnlLogin.Name = "pnlLogin";
-            this.pnlLogin.Size = new System.Drawing.Size(660, 410);
+            this.pnlLogin.Size = new System.Drawing.Size(532, 401);
             this.pnlLogin.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(291, 220);
+            this.btnLogin.Location = new System.Drawing.Point(222, 237);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(75, 23);
             this.btnLogin.TabIndex = 4;
@@ -115,7 +129,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(276, 178);
+            this.lblPassword.Location = new System.Drawing.Point(207, 195);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(56, 13);
             this.lblPassword.TabIndex = 3;
@@ -124,7 +138,7 @@
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(276, 139);
+            this.lblUsername.Location = new System.Drawing.Point(207, 156);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(58, 13);
             this.lblUsername.TabIndex = 2;
@@ -132,7 +146,7 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(279, 194);
+            this.txtPassword.Location = new System.Drawing.Point(210, 211);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(100, 20);
@@ -140,21 +154,82 @@
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(279, 155);
+            this.txtUsername.Location = new System.Drawing.Point(210, 172);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(100, 20);
             this.txtUsername.TabIndex = 0;
+            // 
+            // pnlRegister
+            // 
+            this.pnlRegister.Location = new System.Drawing.Point(12, 434);
+            this.pnlRegister.Name = "pnlRegister";
+            this.pnlRegister.Size = new System.Drawing.Size(532, 407);
+            this.pnlRegister.TabIndex = 2;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(155, 111);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(229, 20);
+            this.lblDescription.TabIndex = 5;
+            this.lblDescription.Text = "Sign in to your AllChat account:";
+            // 
+            // lblNew
+            // 
+            this.lblNew.AutoSize = true;
+            this.lblNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNew.Location = new System.Drawing.Point(194, 131);
+            this.lblNew.Name = "lblNew";
+            this.lblNew.Size = new System.Drawing.Size(71, 16);
+            this.lblNew.TabIndex = 6;
+            this.lblNew.Text = "New user?";
+            // 
+            // lblNewUser
+            // 
+            this.lblNewUser.AutoSize = true;
+            this.lblNewUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblNewUser.Location = new System.Drawing.Point(262, 131);
+            this.lblNewUser.Name = "lblNewUser";
+            this.lblNewUser.Size = new System.Drawing.Size(92, 16);
+            this.lblNewUser.TabIndex = 7;
+            this.lblNewUser.Text = "Register here.";
+            this.lblNewUser.Click += new System.EventHandler(this.lblNewUser_Click);
+            // 
+            // lblInvalid1
+            // 
+            this.lblInvalid1.AutoSize = true;
+            this.lblInvalid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvalid1.ForeColor = System.Drawing.Color.Red;
+            this.lblInvalid1.Location = new System.Drawing.Point(109, 263);
+            this.lblInvalid1.Name = "lblInvalid1";
+            this.lblInvalid1.Size = new System.Drawing.Size(0, 15);
+            this.lblInvalid1.TabIndex = 8;
+            // 
+            // lblInvalid2
+            // 
+            this.lblInvalid2.AutoSize = true;
+            this.lblInvalid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvalid2.ForeColor = System.Drawing.Color.Red;
+            this.lblInvalid2.Location = new System.Drawing.Point(141, 278);
+            this.lblInvalid2.Name = "lblInvalid2";
+            this.lblInvalid2.Size = new System.Drawing.Size(0, 15);
+            this.lblInvalid2.TabIndex = 9;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 440);
+            this.ClientSize = new System.Drawing.Size(556, 440);
+            this.Controls.Add(this.pnlRegister);
             this.Controls.Add(this.pnlLogin);
             this.Controls.Add(this.mnsMain);
             this.MainMenuStrip = this.mnsMain;
+            this.MaximizeBox = false;
             this.Name = "frmMain";
-            this.Text = "Main";
+            this.Text = "AllChat™";
             this.mnsMain.ResumeLayout(false);
             this.mnsMain.PerformLayout();
             this.pnlLogin.ResumeLayout(false);
@@ -178,6 +253,12 @@
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Panel pnlRegister;
+        private System.Windows.Forms.Label lblNewUser;
+        private System.Windows.Forms.Label lblNew;
+        private System.Windows.Forms.Label lblInvalid1;
+        private System.Windows.Forms.Label lblInvalid2;
     }
 }
 
